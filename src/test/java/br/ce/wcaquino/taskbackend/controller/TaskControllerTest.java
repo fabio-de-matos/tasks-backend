77413ea56e0b1a092d738e5e03c1798cacafe0af
fabio.matos <fabio.matos@globalhitss.com.br>
2021-02-27 16:07:42 -0300

    Testes unitários da aplicação

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,88 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+
+public class TaskControllerTest {
+	
+	// O objeto:
+	// "@Autowired
+	// private TaskRepo todoRepo;"
+	// será substituido pelo objeto abaixo
+	// Isto é mock. A simulação de objeto. Isso permite que o teste não precise do Spring e um BD para a execução dos testes.
+	// Isso é importante para deixar o teste mais rápido e isolado de dependêcias externas;
+	@Mock
+	private TaskRepo taskRepo;
+	
+	// O objeto mock "TaskRepo taskRepo" é injetado na classe abaixo substituindo o original 
+	@InjectMocks
+	private TaskController controller;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+
+	}
+
+	@Test
+	public void naoDeveSalvarTarefaSemDescricao() {
+		Task todo = new Task();
+		todo.setDueDate(LocalDate.now());
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria passar por aqui");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the task description", e.getMessage());
+		}
+
+	}
+
+	@Test
+	public void naoDeveSalvarTarefaSemData() {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+//		todo.setDueDate(LocalDate.now());
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria passar por aqui");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the due date", e.getMessage());
+		}
+	}
+
+	@Test
+	public void naoDeveSalvarTarefaSemDataPassada() {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		todo.setDueDate(LocalDate.of(1970, 04, 18));
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria passar por aqui");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Due date must not be in past", e.getMessage());
+		}
+	}
+
+	@Test
+	public void deveSalvarTarefaComSucesso() throws ValidationException {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		todo.setDueDate(LocalDate.now());
+		controller.save(todo);
+		
+		Mockito.verify(taskRepo).save(todo);
+	}
+
+}
---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,28 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+	
+	@Test
+	public void devRetornarTrueParaDatasFuturas() {
+		LocalDate date = LocalDate.of(2030, 04, 18);
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void devRetornarFalseParaDatasPassadas() {
+		LocalDate date = LocalDate.of(2010, 04, 18);
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void devRetornarTrueParaDataPresente() {
+		LocalDate date = LocalDate.now();
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+
+}
